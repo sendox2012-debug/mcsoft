b03a65467ab5f29217daafbb1bac47b424ca2035
sendox2012-debug <sendox2012-debug@users.noreply.github.com>
2026-08-16 15:03:00 +0300

    зай

---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+cd  C:\Users\m9629\Desktop\mcsoft
+call .venv\Scripts\activate.bat
+py main.py
+pause
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/said/Documents/mcsoft
+source .venv/bin/activate
+python main.py
